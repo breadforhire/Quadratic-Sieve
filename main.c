c6989f3e7aab86238250e04e647a0763ceafca0c
Michael Metyk <126725193+breadforhire@users.noreply.github.com>
2023-12-12 13:45:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -18,27 +18,22 @@
      vt = _mm256_permute_ps(vt, _MM_SHUFFLE(i & 3, i & 3, i & 3, i & 3)); \
 })
 
-int ipow(int base, int exp)
-{
-    int result = 1;
-    for (;;)
-    {
-        if (exp & 1)
-            result *= base;
-        exp >>= 1;
-        if (!exp)
-            break;
-        base *= base;
-    }
-
-    return result;
-}
 
 
-__m256 _mm256_powers_ps(float scalar) {
-    return _mm256_setr_ps(
-        scalar, scalar ^ 3, scalar ^ 5, 0, 0, x4 * x2, x4 * x3, x4 * x4);
-}
+
+
+/*these residues will be used for later*/
+typedef struct residue_narrow {
+  __attribute__((__aligned__(16)))
+  int32_t limbs[8];
+} residue_narrow_t;
+
+
+/*taylor expansion */
+
+/*I dont think we can do residue like x^2 = q mod n or x ^*/
+
+
 
 float n  = 2.0f;
 float sqrt_n =  1.41421356237f;
@@ -58,7 +53,17 @@ __attribute__((__aligned__(32)))
 static const float fives[8] = {5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f, 5.0f};
 
 
-
+/*got this on stack exchange broadcast x values then it should be x^1, x^2 ... s on*/
+__m256 _mm256_powers_ps(float x) {
+    float const x2 = x * x, x4 = x2 * x2;
+    return _mm256_mul_ps(
+        _mm256_mul_ps(
+            _mm256_setr_ps( x, x2,  x, x2,  x, x2,  x, x2),
+            _mm256_setr_ps( 1,  1, x2, x2,  1,  1, x2, x2)
+        ),
+            _mm256_setr_ps( 1,  1,  1,  1, x4, x4, x4, x4)
+    );
+}
 
 
 /*we are going to add a primality test to check for the smothness of the prime number*/
@@ -70,13 +75,14 @@ void sieve() {
    /*vectors taht will be used in */
     __m256 taylor;
     __m256 smooth;
-    __m256 log;
 
     __m256 a_i;
     __m256 b_i;
 
     __m256 ones;
     __m256 scalar;
+    __m256 log;
+    float temp[8];
 
 
 
@@ -97,14 +103,18 @@ void sieve() {
      /*this is taylor series of a log function*/
 
      log = _mm256_mul_ps(log, taylor);
+
+     _mm256 scalar = _mm256_loadu_ps(&log[0]);
      PRINT(log);
-     PRINT(p_expansion);
 
 
      a_i = _mm256_add_ps(x, _mm256_set1_ps(sqrt_n));
      b_i = _mm256_sub_ps(_mm256_mul_ps(a_i, a_i), _mm256_set1_ps(n));
 
      /*this sort of works like a broadcast*/
+
+     PRINT(b_i);
+
 
      for(int i = 0; i < 8; i++)
      {
